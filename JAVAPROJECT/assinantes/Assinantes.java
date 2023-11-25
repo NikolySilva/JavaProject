5c3a2e22f9c7def0262842e5b2df8ba075d193df
ana <anacarolineofc130@gmail.com>
2023-11-25 16:20:46 -0300

    Declarando as variáveis da classe Assinante.

---
+++ JAVAPROJECT/assinantes/Assinantes.java
@@ -0,0 +1,10 @@
+package JAVAPROJECT.assinantes;
+import JAVAPROJECT.chamada.Chamada;
+
+ // declarando as variáveis
+public class Assinantes {
+   private long cpf;
+   private String nome;
+   private int numCel;
+   private int numChamadas;
+}
