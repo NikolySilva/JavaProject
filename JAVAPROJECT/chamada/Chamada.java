792f65c00c475bd7b4839aef7da1c8b31fa6e16f
ana <anacarolineofc130@gmail.com>
2023-11-25 16:34:14 -0300

    Declarando variáveis.

---
+++ JAVAPROJECT/chamada/Chamada.java
@@ -0,0 +1,9 @@
+package JAVAPROJECT.chamada;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+ 
+ //Declaramos as variáveis
+public class Chamada {
+    private GregorianCalendar data;
+    private int duracao;
+}
