9f18f422b63ffa034204d577b479fbfbd3a649d0
ana <anacarolineofc130@gmail.com>
2023-11-25 16:39:10 -0300

    classe Chamada finalizada.

--- JAVAPROJECT/chamada/Chamada.java
+++ JAVAPROJECT/chamada/Chamada.java
@@ -6,7 +6,6 @@ import java.util.GregorianCalendar;
 public class Chamada {
     private GregorianCalendar data;
     private int duracao;
-}
 
  // Construtor
     public Chamada (GregorianCalendar data, int duracao) {
@@ -17,4 +16,20 @@ public class Chamada {
     // Método para obter a data (função que vai retornar a data)
     public GregorianCalendar getData() {
         return data;
+}
+
+// Método para obter a duração (função que vai retornar a duração)
+public int getDuracao() {
+    return duracao;
+}
+
+// Método para obter uma representação textual da chamada
+
+@Override
+public String toString() {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    return "Data: " + sdf.format(data.getTime()) + "\nDuracao: " + duracao + " minutos"; 
+    //O código retorna uma string que combina a data formatada usando um objeto SimpleDateFormat chamado sdf e a duração (em minutos). 
+    // A string resultante parece representar informações sobre uma data e sua duração, formatadas de maneira legível.
+}
 }
