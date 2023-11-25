7e7f0a73c504fb1f4144a07d548f9910399136e5
ana <anacarolineofc130@gmail.com>
2023-11-25 16:25:28 -0300

    Add uma array e o construtor na classe Assinantes.

--- JAVAPROJECT/assinantes/Assinantes.java
+++ JAVAPROJECT/assinantes/Assinantes.java
@@ -8,3 +8,15 @@ public class Assinantes {
    private int numCel;
    private int numChamadas;
 }
+
+//Criando uma array
+   private Chamada[] chamadas;
+
+ //Construtor
+   public Assinantes (long cpf, String nome, int numCel, int numChamadas) {
+      this.cpf = cpf;
+      this.nome = nome;
+      this.numCel = numCel;
+      this.chamadas = new Chamada[numChamadas];
+      this.numChamadas = 0;
+   }
